b49503ee8310a915c24aedb17bf829096ae0eea4
newajDev <newaj.design@gmail.com>
2026-07-17 16:37:14 +0600

    root api added

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 const schema = z.object({
-  PORT: z.string().default("5000"),
+  PORT: z.coerce.number().default(4000),
   FRONTEND_URL: z.string().url(),
   BACKEND_URL: z.string().url(),
   DATABASE_URL: z.string().url(),
-  CORS_ORIGINS: z.string().default("http://localhost:3000")
+  CORS_ORIGINS: z.string().default("http://localhost:3000"),
 });
 
 export const env = schema.parse(process.env);
