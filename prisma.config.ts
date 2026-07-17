dac8c2a3ab0f5c29ccefae00755162bfd2db5e85
newajDev <newaj.design@gmail.com>
2026-07-17 15:29:06 +0600

    prisma updated 4

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "prisma/config";
 import { env } from "./src/config/env";
 
 export default defineConfig({
-  schema: "prisma/schema",
+  schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
